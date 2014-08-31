345c234384d006bdca5c5f88ac97ac2a1e6403c6
Alex Wilson <alexwilson2012@u.northwestern.edu>
2014-08-31 10:01:30 -0700

    Cleaned up references and absolute paths

--- functions.php
+++ functions.php
@@ -19,3 +19,42 @@ function youandi_wp_title( $title, $sep ) {
 	return $title;
 }
 add_filter( 'wp_title', 'youandi_wp_title', 10, 2 );
+
+
+class ResourceManager {
+	private static $store;
+
+	const IMAGE_PATH = 'wp-content/themes/youandi/images/';
+
+	public static function add($name, $type) {
+		if (!isset(self::$store)) {
+			self::$store = array(
+				'js' => array(),
+				'css' => array(),
+			);
+		}
+
+		if (!in_array($type, array('js', 'css'))) {
+			// print warning message function
+			// youandi_warn("$name not passed valid js or css type: $type");
+		}
+
+		$root_path = get_bloginfo('template_directory');
+		self::$store[$type][] = "$root_path/$name";
+	}
+
+	public static function getTags($type) {
+		$tag_string = '';
+		foreach (self::$store[$type] as $path) {
+			if ($type === 'css') {
+				$tag_string .= "<link rel='stylesheet' type='text/css' href='$path'>";
+			} else if ($type === 'js') {
+				$tag_string .= "<script type='text/javascript' src='$path'></script>";
+			}
+		}
+		return $tag_string;
+	}
+}
+
+
+ResourceManager::add('css/header.css', 'css');
