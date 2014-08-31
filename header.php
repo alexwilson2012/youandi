f51a4d32ca53cddcc17ba0d744236f274729440d
Alex Wilson <AlexWilson2012@u.northwestern.edu>
2014-08-30 18:58:35 -0700

    Update header.php

--- header.php
+++ header.php
@@ -32,6 +32,7 @@
 			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
 				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+				<span>The bestest blog ever!</span>
 			</a>
 		</header><!-- #masthead -->
 
