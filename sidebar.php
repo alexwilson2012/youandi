07c7d08411931b668c579630011c86051c04ccc1
Alex Wilson <alexwilson2012@u.northwestern.edu>
2014-09-14 17:15:58 -0700

    Sidebar icons

--- sidebar.php
+++ sidebar.php
@@ -4,10 +4,26 @@
 		<div class="yai-email-wrap yai-bgcolor-mint">
 			<input type="text" class="yai-email-subscribe yai-input" placeholder="email">
 			<div class="yai-social-wrap">
-				<div class="yai-social">f</div>
-				<div class="yai-social">f</div>
-				<div class="yai-social">f</div>
-				<div class="yai-social">f</div>
+				<div class="yai-social">
+					<a href="">
+						<img src="<?php echo ResourceManager::getImg('icons/facebook3.png');?>">
+					</a>
+				</div>
+				<div class="yai-social">
+					<a href="">
+						<img src="<?php echo ResourceManager::getImg('icons/pinterest.png');?>">
+					</a>
+				</div>
+				<div class="yai-social">
+					<a href="">
+						<img src="<?php echo ResourceManager::getImg('icons/twitter.png');?>">
+					</a>
+				</div>
+				<div class="yai-social">
+					<a href="">
+						<img src="<?php echo ResourceManager::getImg('icons/instagram.png');?>">
+					</a>
+				</div>
 			</div>
 		</div>
 	</div>
